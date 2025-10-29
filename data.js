9b6badda5b6e30876a9111bab2326de9aa8b4613
panaleewut-ui <panalee.wut@gmail.com>
2025-10-29 14:50:35 +0700

    Create data.js

---
+++ data.js
@@ -0,0 +1,63 @@
+// data.js
+const foodPlans = [
+  {
+    energyRange: [1000, 1200],
+    proteinRange: [40, 45],
+    portions: [
+      { type: "ข้าว-แป้ง", total: 6 },
+      { type: "เนื้อสัตว์", total: 3 },
+      { type: "ผัก", total: 4 },
+      { type: "ผลไม้", total: 2 },
+      { type: "นม", total: 1 },
+      { type: "ไขมัน", total: 2 }
+    ]
+  },
+  {
+    energyRange: [1201, 1400],
+    proteinRange: [45, 50],
+    portions: [
+      { type: "ข้าว-แป้ง", total: 7 },
+      { type: "เนื้อสัตว์", total: 3 },
+      { type: "ผัก", total: 4 },
+      { type: "ผลไม้", total: 2 },
+      { type: "นม", total: 1 },
+      { type: "ไขมัน", total: 2 }
+    ]
+  },
+  {
+    energyRange: [1401, 1600],
+    proteinRange: [50, 55],
+    portions: [
+      { type: "ข้าว-แป้ง", total: 8 },
+      { type: "เนื้อสัตว์", total: 4 },
+      { type: "ผัก", total: 4 },
+      { type: "ผลไม้", total: 3 },
+      { type: "นม", total: 1 },
+      { type: "ไขมัน", total: 3 }
+    ]
+  },
+  {
+    energyRange: [1601, 1800],
+    proteinRange: [55, 60],
+    portions: [
+      { type: "ข้าว-แป้ง", total: 9 },
+      { type: "เนื้อสัตว์", total: 4 },
+      { type: "ผัก", total: 4 },
+      { type: "ผลไม้", total: 3 },
+      { type: "นม", total: 1 },
+      { type: "ไขมัน", total: 3 }
+    ]
+  },
+  {
+    energyRange: [1801, 2200],
+    proteinRange: [60, 75],
+    portions: [
+      { type: "ข้าว-แป้ง", total: 10 },
+      { type: "เนื้อสัตว์", total: 5 },
+      { type: "ผัก", total: 5 },
+      { type: "ผลไม้", total: 4 },
+      { type: "นม", total: 1 },
+      { type: "ไขมัน", total: 4 }
+    ]
+  }
+];
